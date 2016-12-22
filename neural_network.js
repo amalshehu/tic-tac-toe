f477d81177506f2ed24dadbfd079ec52ce0494df
amalshehu <amalshehu@gmail.com>
2016-12-22 23:57:38 +0530

    Callback function

--- neural_network.js
+++ neural_network.js
@@ -21,7 +21,27 @@ var Neural = (function(Neural)){
     //Define weights
     node.weights = [typeof nodes === 'undefined' ? new Array[sizes[layerIndex +1]]
     nodes[layerIndex][index].weights.map(function (w)) {
+       return w;
+    }
+    return node;
+  }
 
+  funtion Net(sizeOrNodes){
+    var sizes, nodes
+    if (Array.isArray(sizeOrNodes) && Array.isArray(sizeOrNodes)[])){
+      sizes = getSizes(sizeOrNodes)
+      nodes = sizesOrNodes
+    }
+    else{
+      sizes = sizesOrNodes
+    }
+    // Function callback
+    this.nodes = sizes.map(function(sizes, i)){
+      var layer = new Array(size);
+      for (var j =0; j<size; ++j){
+        layer[j] = makeNode(i, j, sizes, nodes)
+      }
+      return layer;
     }
   }
 
